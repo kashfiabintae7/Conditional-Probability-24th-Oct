b7b4b97991bb4e0526a8d75b17cb8f46137d0a90
Kashfia <kashfiabintae@gmail.com>
2025-10-30 19:24:03 +0600

    h

--- redball.py
+++ redball.py
@@ -1,6 +1,6 @@
 import random
 
-balls = ['White', 'White', 'Blue', 'Blue', 'Green', 'Green']
+balls = ['Red', 'Red', 'Blue', 'Blue', 'Green', 'Green']
 
 first_ball = random.choice(balls)
 balls.remove(first_ball)
@@ -10,10 +10,10 @@ second_ball = random.choice(balls)
 print('First ball drawn:', first_ball)
 print('Second ball drawn:', second_ball)
 
-if first_ball == 'White':
-    remaining_red = balls.count('White')
+if first_ball == 'Red':
+    remaining_red = balls.count('Red')
     total_remaining = len(balls)
     probability = remaining_red/total_remaining
-    print(f'\nConditional Probability (2nd White | 1st White) = {probability:.2f}')
+    print(f'\nConditional Probability (2nd Red | 1st Red) = {probability:.2f}')
 else:
-    print('\nSince first ball is not White, the condition doesnt apply.') 
+    print('\nSince first ball is not Red, the condition doesnt apply.') 
